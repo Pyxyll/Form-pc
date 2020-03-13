29adc89d437b44b18baf2279d663b7d1e150ac4e
Alex <alexsmith11nd@gmail.com>
2020-03-13 22:05:05 +0000

    Basic structure with nested components added

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+/**
+ * Components
+ */
+import Header from '../components/header.js';
+
+const About = () => (
+    <div>
+        <Header />
+        <p>About page</p>
+    </div>
+)
+export default About
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
+import Link from 'next/link';
+
+/**
+ * Components
+ */
+import Layout from '../components/layout.js';
+
 const Index = () => (
-    <div>
-      <p>Hello Next.js, this is your friend Alex</p>
-    </div>
-  )
-  export default Index
+  <div>
+    {/* <Header /> */}
+    
+    {/* <Link href="/about">
+      <a title="About Page">About Page</a>
+    </Link> */}
+
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+  </div>
+)
+export default Index
