4fdbce99a64dceb4d6c4987519f2fbf1ff15d6ef
Alex <alexsmith11nd@gmail.com>
2020-03-13 18:47:33 +0000

    :)

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 const Index = () => (
     <div>
-      <p>Hello Next.js, this is your friend Brian from logrocket</p>
+      <p>Hello Next.js, this is your friend Alex</p>
     </div>
   )
   export default Index
