e71ecfc44499892b8ad6111b990c718c9fc0fa22
Alex <alexsmith11nd@gmail.com>
2020-03-23 15:52:30 +0000

    ENV file added

--- server/server.js
+++ server/server.js
@@ -1,37 +1,29 @@
-const express = require('express');
-const next = require('next');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const port = process.env.PORT || 5000;
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const server = express();
+const showRoutes = require("./routes/index.js");
 
-app
-  .prepare()
-  .then(() => {
-    const server = express();
-    const showRoutes = require('./routes/index.js');
+// Use CORS
+server.use(cors());
 
-    // API startpoint
-    server.use('/api', showRoutes);
+// API startpoint
+server.use("/api", showRoutes);
 
-    // Standard API. Use above and above file for actual routes.
-    // REMOVE FOR PRODUCTION
-    server.get('/api/parts', (req, res) => {
-      console.log('api hit');
-      res.send({ express_says: 'Hello' });
-    });
+// Standard API. Use above and above file for actual routes.
+// REMOVE FOR PRODUCTION
+server.get("/api/parts", (req, res) => {
+  console.log("api hit");
+  res.send({ express_says: "Hello" });
+});
 
-    server.all('*', (req, res) => {
-      return handle(req, res);
-    });
+server.listen(process.env.PORT, err => {
+  if (err) throw err;
+  console.log(`> Ready on http://localhost:${process.env.PORT}`);
+});
 
-    server.listen(port, err => {
-      if (err) throw err;
-      console.log(`> Ready on http://localhost:${port}`);
-    });
-  })
-  .catch(ex => {
-    console.error(ex.stack);
-    process.exit(1);
-  });
+// .catch(ex => {
+//   console.error(ex.stack);
+//   process.exit(1);
+// });
