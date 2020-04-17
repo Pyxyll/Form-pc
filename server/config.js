e6b63fe2831ed639adbabf6a0587a657bcce912c
Alex Smith <alexsmith11nd@gmail.com>
2020-04-17 20:21:24 +0100

    added various gpu operations or requests to config

--- server/config.js
+++ server/config.js
@@ -1,6 +1,10 @@
 require("dotenv").config({ path: "../.env" });
 
-const { Pool } = require('pg')
+/**
+ * Contains all default db operations for GPU's
+ */
+
+const { Pool } = require('pg');
 const isProduction = process.env.NODE_ENV === 'production'
 
 // const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
@@ -12,15 +16,80 @@ const database = process.env.DB_DATABASE;
 const pw = process.env.DB_PASSWORD;
 const port = process.env.DB_PORT;
 
-// redo connection string on production
+// redo connectionString on production
 const pool = new Pool({
 //   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 //   ssl: isProduction,
-    user: user,
-    host: host,
-    database: database,
-    password: pw,
-    port: port,
+  user: user,
+  host: host,
+  database: database,
+  password: pw,
+  port: port,
 })
 
-module.exports = { pool }
+const getGpus = (req, res) => {
+  pool.query('SELECT * FROM gpus ORDER BY id ASC', (err, results) => {
+    if(err) {
+      throw err;
+    }
+    res.status(200).json(res.rows);
+  })
+}
+
+const getGpuById = (req, res) => {
+  const id = parseInt(req.params.id)
+  
+  pool.query('SELECT * FROM gpus WHERE id = $1', [id], (err, res) => {
+    if (err) {
+      throw err
+    }
+    res.status(200).json(res.rows)
+  })
+}
+
+const createGpu = (req, res) => {
+  // Get the data from the request body
+  const { name, designer, manufacturer, type, length, memory_type } = req.body
+  
+  // Add data to db
+  pool.query('INSERT INTO gpus (name, designer, manufacturer, type, length, memory_type) VALUES ($1, $2, $3, $4, $5, $6)', [name, designer, manufacturer, type, length, memory_type], (err, res) => {
+    if (err) {
+      throw err
+    }
+    res.status(201).send(`GPU added with ID: ${res.insertId}`)
+  })
+}
+
+// const updateGpu = (req, res) => {
+//   const id = parseInt(req.params.id)
+//   const { name, email } = req.body
+
+//   pool.query(
+//     'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+//     [name, email, id],
+//     (err, res) => {
+//       if (err) {
+//         throw err
+//       }
+//       res.status(200).send(`GPU modified with ID: ${id}`)
+//     }
+//   )
+// }
+
+const deleteGpu = (req, res) => {
+  const id = parseInt(req.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (err, res) => {
+    if (err) {
+      throw err
+    }
+    res.status(200).send(`GPU deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  getGpus,
+  getGpuById,
+  createGpu,
+  deleteGpu
+}
